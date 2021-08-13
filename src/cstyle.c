1e13ed0545cbca6c02369f12b42d5124ff4a1d06
yamunan-bitset <yamunan.bitset@gmail.com>
2021-08-13 17:00:21 +0000

    updated error to die

--- src/cstyle.c
+++ src/cstyle.c
@@ -5,12 +5,14 @@
 
 #include "colours.h"
 
+extern void die(const char*); // die.c
+
 void cstyle_list()
 {
   DIR*           dir;
   struct dirent* dirp;
   if ((dir = opendir("*")) == NULL)
-    { fprintf(stderr, red"Failed to open directory"); exit(-1); }
+    { die("Failed to open directory"); exit(-1); }
   while ((dirp = readdir(dir)) != NULL) printf("%s\n", dirp->d_name);
 
   closedir(dir);
